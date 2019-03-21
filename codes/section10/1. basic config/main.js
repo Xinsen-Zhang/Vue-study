07d11b6dc9a889f7265f884a2e02f310ddc16f0a
xinsen <13051885033@163.com>
2019-03-21 09:56:12 +0800

    module => rules

---
+++ codes/section10/1. basic config/main.js
@@ -0,0 +1,4 @@
+// import css
+import './style.css'
+// DOM operation
+document.getElementById('app').innerHTML = 'Hello World'
--- codes/section10/1. basic config/webpack.config.js
+++ codes/section10/1. basic config/webpack.config.js
@@ -1,4 +1,34 @@
+var path = require('path')
 var config = {
-    
+    // 入口的设置
+    entry: {
+        // 入口的 js 文件路径
+        main: './main'
+    },
+    // 出口的设置
+    output: {
+        // 输出的保存路径
+        path: path.join(__dirname, './dist'),
+        // 静态资源的路径, 可以是 url
+        publicPath: '/dist/',
+        // 输出的文件名称
+        filename: 'main.js'
+    },
+    // module 的配置
+    module: {
+        // 规则的配置
+        rules: [
+            // CSS
+            {
+                // 正则表达式来确定文件. require 和 import
+                test: /\.css$/,
+                // loader 的使用, 字符串或者数组. 数组从后往前
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
 }
 module.exports = config
