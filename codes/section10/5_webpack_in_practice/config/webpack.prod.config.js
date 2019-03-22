03e0a528d249ec6e2a0df40bf63ece38202d517b
xinsen <13051885033@163.com>
2019-03-22 23:21:46 +0800

    clean-webpack-plugin

--- codes/section10/5_webpack_in_practice/config/webpack.prod.config.js
+++ codes/section10/5_webpack_in_practice/config/webpack.prod.config.js
@@ -1,8 +1,13 @@
+var path = require('path');
 var config = require('./webpack.base.config');
 var merge = require('webpack-merge');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 var UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); 
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+config.output.filename = 'index.[hash].js';
 
 module.exports = merge(config, {
     mode: 'production',
@@ -23,10 +28,24 @@ module.exports = merge(config, {
         }
     ]},
     plugins:[
+        // extract css file
         new MiniCssExtractPlugin({
             filename: '[name].[hash].css',
             chunkFilename: '[id].[hash].css'
-        })
+        }),
+        // generate a html file with link:css and scrpt:src
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, '../index.html'),
+            title: 'html-webpack-plugin 的使用',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeAttributeQuotes: true
+            }
+        }),
+        // clean files
+        new CleanWebpackPlugin()
     ],
     optimization:{minimizer:[
         new OptimizeCssAssetsWebpackPlugin({}),
