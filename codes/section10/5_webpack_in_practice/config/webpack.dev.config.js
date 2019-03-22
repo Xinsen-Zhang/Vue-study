c8d62f1e90b0c6954cb26eb69ed794fdab18657e
xinsen <13051885033@163.com>
2019-03-22 16:55:52 +0800

    sass

--- codes/section10/5_webpack_in_practice/config/webpack.dev.config.js
+++ codes/section10/5_webpack_in_practice/config/webpack.dev.config.js
@@ -1,21 +1,28 @@
+// import config from './webpack.base.config';
+// import merge from 'webpack-merge';
 var config = require('./webpack.base.config');
 var merge = require('webpack-merge');
-
 
 module.exports = merge(config, {
     module:{
         rules: [
             {
                 // css
-                test: /\.css$/,
+                test: /\.(c|sc|sa)ss$/,
                 use: [
                     'style-loader', // 样式的注入
                     {loader: 'css-loader',
                     options: {
                         sourceMap: true
-                    }} // css 样式的识别
+                    }},
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
                 ]
-            }
+            },
             
         ]
     }
--- codes/section10/5_webpack_in_practice/src/index.js
+++ codes/section10/5_webpack_in_practice/src/index.js
@@ -1,8 +1,12 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import './index.css';
+import './index.sass'
 function appendDiv() {
     // let div = documment.createElement('div')
     var div = document.createElement('div');
     div.innerHTML = _.join(['你好', 'js 开发者'], ' ');
+    div.className = 'box';
+    div.classList.add('boxes');
     document.body.append(div);
 }
 appendDiv();
